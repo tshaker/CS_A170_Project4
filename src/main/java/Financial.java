f939ba8cf4b2d6b5501d1330be031198b816e4a8
Tim Shaker <tim@tmshkr.com>
2024-05-20 15:10:03 -0700

    add javadocs

--- src/main/java/Financial.java
+++ src/main/java/Financial.java
@@ -1,5 +1,12 @@
 public class Financial {
-    public static double percentOf(double interestRate, double balance) {
-        return (interestRate / 100.0) * balance;
-    }
+	/**
+	 * Returns the amount of interest earned on a given balance.
+	 * 
+	 * @param interestRate the interest rate
+	 * @param balance      the account balance
+	 * @return the amount of interest earned
+	 */
+	public static double percentOf(double interestRate, double balance) {
+		return (interestRate / 100.0) * balance;
+	}
 }
