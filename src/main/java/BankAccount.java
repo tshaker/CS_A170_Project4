1cddd0f5c30cc7b99e479517966418aae964dda4
Tim Shaker <tim@tmshkr.com>
2024-05-20 15:07:36 -0700

    add javadocs

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,17 +1,54 @@
+/**
+ * Class representing a checking or savings account.
+ */
 public class BankAccount {
 
+    /**
+     * The balance of the bank account.
+     */
     private double balance;
+
+    /**
+     * The account's name.
+     */
     private String accName;
+
+    /**
+     * The account number.
+     */
     private int accNum;
+
+    /**
+     * The account type: Checking (C) or Savings (S).
+     */
     private char accountType;
+
+    /**
+     * The interest rate for a savings account, expressed as percentage points,
+     * e.g., 4.0 for a 4% interest rate.
+     */
     private double interestRate;
 
+    /**
+     * The last assigned account number.
+     */
     private static int lastAccNum = 0;
 
+    /**
+     * Creates a new instance of BankAccount and increments the lastAccNum.
+     */
     public BankAccount() {
         this.accNum = ++lastAccNum;
     }
 
+    /**
+     * Creates a new instance of BankAccount with the specified parameters and
+     * increments the lastAccNum.
+     * 
+     * @param balance     the starting balance of the account
+     * @param accName     the account name
+     * @param accountType the account type
+     */
     public BankAccount(double balance, String accName, char accountType) {
         setAccountType(accountType);
         this.balance = balance;
@@ -19,6 +56,12 @@ public class BankAccount {
         this.accNum = ++lastAccNum;
     }
 
+    /**
+     * Withdraws the specified amount from the bank account.
+     * 
+     * @param amount the amount to withdraw.
+     * @throws IllegalArgumentException if the account has insufficient funds
+     */
     public void withdraw(double amount) {
         double newBalance = balance - amount;
         if (newBalance >= 0) {
@@ -28,6 +71,11 @@ public class BankAccount {
         }
     }
 
+    /**
+     * Deposits the specified amount from the bank account.
+     * 
+     * @param amount the amount to deposit.
+     */
     public void deposit(double amount) {
         balance += amount;
     }
@@ -39,6 +87,11 @@ public class BankAccount {
         System.out.println("The account interest rate is: " + getInterestRate());
     }
 
+    /**
+     * Adds interest to the account balance. Only savings accounts bear interest.
+     * 
+     * @throws IllegalArgumentException if the account type is not 'S'
+     */
     public void addInterest() {
         if (accountType == 'S') {
             double amount = Financial.percentOf(interestRate, balance);
@@ -48,28 +101,58 @@ public class BankAccount {
         }
     }
 
+    /**
+     * Returns the balance of the account..
+     * 
+     * @return the account balance
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Sets the balance of the account.
+     * 
+     * @param balance the new balance
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Returns the account name.
+     * 
+     * @return the name of the account
+     */
     public String getName() {
         return accName;
     }
 
+    /**
+     * Sets the name of the account.
+     * 
+     * @param accName the new name
+     */
     public void setName(String accName) {
         this.accName = accName;
     }
 
+    /**
+     * Returns the account type.
+     * 
+     * @return the type of the account
+     */
     public char getAccountType() {
         return accountType;
     }
 
+    /**
+     * Sets the account type. Only 'S' for savings and 'C' for checking are allowed.
+     * 
+     * @param accountType the new account type
+     * @throws IllegalArgumentException if the account type is invalid
+     */
     public void setAccountType(char accountType) {
-        this.accountType = accountType;
         switch (accountType) {
             case 'S':
                 this.interestRate = 3;
@@ -80,16 +163,35 @@ public class BankAccount {
             default:
                 throw new IllegalArgumentException("Invalid account type");
         }
+        this.accountType = accountType;
     }
 
+    /**
+     * Returns the interest rate of the account.
+     * 
+     * @return the interest rate
+     */
     public double getInterestRate() {
         return interestRate;
     }
 
+    /**
+     * Sets the interest rate of the account.
+     * 
+     * @param interestRate the new interest rate
+     */
     public void setInterestRate(double interestRate) {
         this.interestRate = interestRate;
     }
 
+    /**
+     * Performs a transfer of funds from one account to another.
+     * 
+     * @param fromAccount the account to transfer funds from
+     * @param toAccount   the account to transfer funds to
+     * @param amount      the amount to transfer
+     * @throws IllegalArgumentException if the fromAccount has insufficient funds
+     */
     public static void transfer(BankAccount fromAccount, BankAccount toAccount, double amount) {
         double newBalance = fromAccount.getBalance() - amount;
         if (newBalance >= 0) {
