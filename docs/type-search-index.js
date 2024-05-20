69627bd99d49d12cb47d8ea76e11876ffa25b18c
Tim Shaker <tim@tmshkr.com>
2024-05-20 15:41:31 -0700

    add generated javadocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BankAccount"},{"p":"<Unnamed>","l":"BankAccountDemo"},{"p":"<Unnamed>","l":"Financial"}];updateSearchResults();
